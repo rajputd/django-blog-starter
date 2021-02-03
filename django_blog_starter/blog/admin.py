8fb8f92ab2ad68a99bf2ace3108237660494102e
Dileep Rajput <dileeprajput686@gmail.com>
2021-02-02 22:43:12 -0500

    Add models to admin page

--- django_blog_starter/blog/admin.py
+++ django_blog_starter/blog/admin.py
@@ -1,3 +1,12 @@
+from .models import Comment, Post
 from django.contrib import admin
 
 # Register your models here.
+class CommentInline(admin.StackedInline):
+  model = Comment
+
+class PostAdmin(admin.ModelAdmin):
+  inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
