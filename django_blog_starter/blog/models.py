0b29935e633248b78e33c72ed1dd75867775a33f
Dileep Rajput <dileeprajput686@gmail.com>
2021-02-03 20:40:37 -0500

    Display snippets on index page instead of the entire post

--- django_blog_starter/blog/models.py
+++ django_blog_starter/blog/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+snippet_length = 200
+
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=300)
   pub_date = models.DateField('date published', default=timezone.now)
   content = models.TextField()
+
+  def getSnippet(self):
+    if len(self.content) < snippet_length:
+      return self.content
+    else:
+      return self.content[0:snippet_length - 3] + "..."
 
   def __str__(self):
       return self.title
