fbd7a1ca7a95a07f82b82b53747ec61461a9d814
Dileep Rajput <dileeprajput686@gmail.com>
2021-02-03 20:21:14 -0500

    Make a page to show all articles

--- django_blog_starter/blog/views.py
+++ django_blog_starter/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-  return render(request, 'blog/index.html')
+  all_posts = Post.objects.all()
+  context = {
+    'posts': all_posts
+  }
+  return render(request, 'blog/index.html', context)
