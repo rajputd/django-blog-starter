4e30a8be1ba582f32970b6e818438e2d0dcc3cd5
Dileep Rajput <dileeprajput686@gmail.com>
2021-02-07 15:06:56 -0500

    Add comment form

--- django_blog_starter/blog/views.py
+++ django_blog_starter/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Post
+from django.utils import timezone
+
+from .models import Comment, Post
 
 # Create your views here.
 def index(request):
@@ -12,3 +14,24 @@ def index(request):
 def post(request, post_id):
   post = get_object_or_404(Post, pk=post_id)
   return render(request, 'blog/post.html', {'post' : post})
+
+def submit_comment(request, post_id):
+  post = get_object_or_404(Post, pk=post_id)
+  try:
+    comment_content = request.POST['content']
+    comment_author = request.POST['author']
+  except (KeyError) as e:
+    return render(request, 'blog/post.html', {
+      'post' : post,
+      'error_message': 'Please fill out the comment form completely'
+    })
+  else:
+    post.comment_set.create(
+      content = comment_content,
+      author=comment_author,
+      pub_date=timezone.now()
+    )
+    return render(request, 'blog/post.html', {
+      'post' : post,
+    })
+
